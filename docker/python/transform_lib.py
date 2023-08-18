ab3c7340a241c8e9c2cfb3a5062f973ab7f4f198
aarshee04 <ramoksmi@gmail.com>
2023-08-18 13:11:59 -0400

    syncing tested programs

--- docker/python/transform_lib.py
+++ docker/python/transform_lib.py
@@ -1,4 +1,3 @@
-
 def transform(val, column, ssn_xref, lob_xref):
     retVal = str(val)
 
@@ -6,16 +5,44 @@ def transform(val, column, ssn_xref, lob_xref):
         for trans in column["transformations"]:
             if trans["function"] == "mask":
                 retVal = mask(retVal, trans)
+            elif trans["function"] == "length":
+               retVal = length(retVal, trans, column["justification"])
             elif trans["function"] == "trim":
                 retVal = trim(retVal, trans)
             elif trans["function"] == "ssn_lookup":
                 retVal = ssnLookup(retVal, trans, ssn_xref)
             elif trans["function"] == "lob_lookup":
                 retVal = lobLookup(retVal, trans, lob_xref)
-            elif trans["function"] == "deduction_lookup":
+            elif trans["function"] == "payroll_deduct_lookup":
                 retVal = deductionLookup(retVal, trans, lob_xref)
 
     return retVal
+
+
+def get_column_format(transform, justification):
+    # routine to take only the specified length based on the justification (fixed with length)
+    width = str(transform["parameters"]["length"])
+    alignment = "<"
+
+    if justification == 1:
+        alignment = "^"
+    if justification == 2:
+        alignment = ">"
+    
+    return "{:" + "{alignment}{width}.{width}".format(alignment=alignment, width=width) + "}"
+
+
+def length(val, transform, justification):
+    # routine to take only the specified length based on the justification
+    width = int(transform["parameters"]["length"])
+    
+    if justification == 0:
+        return str(val).ljust(width)
+    if justification == 1:
+        return str(val).center(width)
+    if justification == 2:
+        return str(val).rjust(width)
+
 
 def mask(val, transform):
     # convert str to list of chars as str object is immutable
@@ -31,6 +58,7 @@ def mask(val, transform):
 
     return "".join(retVal)
 
+
 def trim(val, transform):
     retVal = val
 
@@ -40,29 +68,32 @@ def trim(val, transform):
         retVal = str(val).rstrip()
     elif "left" in transform["parameters"]:
         retVal = str(val).lstrip()
-    
+
     return retVal
 
+
 def ssnLookup(key, transform, ssn_xref):
-    retVal = key
-    
-    if(key in ssn_xref):
+    retVal = ""
+
+    if (key in ssn_xref):
         retVal = ssn_xref[key]
 
     return retVal
 
+
 def lobLookup(key, transform, lob_xref):
     retVal = key
-    
-    if(key in lob_xref):
+
+    if (key in lob_xref):
         retVal = lob_xref[key]["code"]
 
     return retVal
 
+
 def deductionLookup(key, transform, lob_xref):
     retVal = key
-    
-    if(key in lob_xref):
+
+    if (key in lob_xref):
         retVal = lob_xref[key]["deduction"]
 
     return retVal
